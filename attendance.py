c2039febbd0b2eca08b018dafa853408c15289d9
sewon-31 <oneoctober12@gmail.com>
2025-09-22 10:12:35 +0900

    method 분리

--- attendance.py
+++ attendance.py
@@ -1,96 +1,104 @@
-id1 = {}
+members_id = {}
 id_cnt = 0
 
 # dat[사용자ID][요일]
-dat = [[0] * 100 for _ in range(100)]
-points = [0] * 100
+attendance_cnt_by_wday = [[0] * 100 for _ in range(100)]
+attendance_points = [0] * 100
 grade = [0] * 100
 names = [''] * 100
-wed = [0] * 100
-weeken = [0] * 100
+wednesday_attendance_cnts = [0] * 100
+weekend_attendance_cnts = [0] * 100
 
-def input2(w, wk):
+
+def input2(name, wday):
     global id_cnt
 
-    if w not in id1:
+    if name not in members_id:
         id_cnt += 1
-        id1[w] = id_cnt
-        names[id_cnt] = w
+        members_id[name] = id_cnt
+        names[id_cnt] = name
 
-    id2 = id1[w]
+    add_points(name, wday)
 
-    add_point = 0
-    index = 0
 
-    if wk == "monday":
+def add_points(name, wday):
+    id2 = members_id[name]
+
+    if wday == "monday":
         index = 0
-        add_point += 1
-    elif wk == "tuesday":
+        point = 1
+    elif wday == "tuesday":
         index = 1
-        add_point += 1
-    elif wk == "wednesday":
+        point = 1
+    elif wday == "wednesday":
         index = 2
-        add_point += 3
-        wed[id2] += 1
-    elif wk == "thursday":
+        point = 3
+    elif wday == "thursday":
         index = 3
-        add_point += 1
-    elif wk == "friday":
+        point = 1
+    elif wday == "friday":
         index = 4
-        add_point += 1
-    elif wk == "saturday":
+        point = 1
+    elif wday == "saturday":
         index = 5
-        add_point += 2
-        weeken[id2] += 1
-    elif wk == "sunday":
+        point = 2
+    elif wday == "sunday":
         index = 6
-        add_point += 2
-        weeken[id2] += 1
+        point = 2
 
-    dat[id2][index] += 1
-    points[id2] += add_point
+    attendance_cnt_by_wday[id2][index] += 1
+    attendance_points[id2] += point
 
-def input_file():
+    if wday in ["wednesday"]:
+        wednesday_attendance_cnts[id2] += 1
+
+    if wday in ["saturday", "sunday"]:
+        weekend_attendance_cnts[id2] += 1
+
+
+def input_file(list_file="attendance_weekday_500.txt"):
     try:
-        attendance_list = "attendance_weekday_500.txt"
-        with open(attendance_list, encoding='utf-8') as f:
-            for _ in range(500):
-                line = f.readline()
-                if not line:
-                    break
-                parts = line.strip().split()
-                if len(parts) == 2:
-                    input2(parts[0], parts[1])
-
-        for i in range(1, id_cnt + 1):
-            if dat[i][2] > 9:
-                points[i] += 10
-            if dat[i][5] + dat[i][6] > 9:
-                points[i] += 10
-
-            if points[i] >= 50:
-                grade[i] = 1
-            elif points[i] >= 30:
-                grade[i] = 2
-            else:
-                grade[i] = 0
-
-            print(f"NAME : {names[i]}, POINT : {points[i]}, GRADE : ", end="")
-            if grade[i] == 1:
-                print("GOLD")
-            elif grade[i] == 2:
-                print("SILVER")
-            else:
-                print("NORMAL")
-
-        print("\nRemoved player")
-        print("==============")
-        for i in range(1, id_cnt + 1):
-            if grade[i] not in (1, 2) and wed[i] == 0 and weeken[i] == 0:
-                print(names[i])
-
+        with open(list_file, encoding='utf-8') as f:
+            attendance_list = f.readlines()
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
+    return attendance_list
+
+
+def analyze_attendance(attendance_list: list[str]):
+    for line in attendance_list:
+        parts = line.strip().split()
+        if len(parts) == 2:
+            input2(parts[0], parts[1])
+
+    for i in range(1, id_cnt + 1):
+        if attendance_cnt_by_wday[i][2] > 9:
+            attendance_points[i] += 10
+        if attendance_cnt_by_wday[i][5] + attendance_cnt_by_wday[i][6] > 9:
+            attendance_points[i] += 10
+
+        if attendance_points[i] >= 50:
+            grade[i] = 1
+        elif attendance_points[i] >= 30:
+            grade[i] = 2
+        else:
+            grade[i] = 0
+
+        print(f"NAME : {names[i]}, POINT : {attendance_points[i]}, GRADE : ", end="")
+        if grade[i] == 1:
+            print("GOLD")
+        elif grade[i] == 2:
+            print("SILVER")
+        else:
+            print("NORMAL")
+
+    print("\nRemoved player")
+    print("==============")
+    for i in range(1, id_cnt + 1):
+        if grade[i] not in (1, 2) and wednesday_attendance_cnts[i] == 0 and weekend_attendance_cnts[i] == 0:
+            print(names[i])
+
 
 if __name__ == "__main__":
-    input_file()
+    attendance_list = input_file()
+    analyze_attendance(attendance_list)
--- test_attendance_manager.py
+++ test_attendance_manager.py
@@ -2,8 +2,9 @@ import pytest
 import attendance
 
 def test_print_result(capsys):
+    attendance_list = attendance.input_file()
+    attendance.analyze_attendance(attendance_list)
 
-    attendance.input_file()
     with open("expected_result.txt", "r") as f:
         expected = f.read()
     captured = capsys.readouterr()
